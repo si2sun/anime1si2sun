65b4b2b554f377d40e1272578ca74cd923549111
si2sun <lizongyan928@gmail.com>
2025-07-23 20:09:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ templates = Jinja2Templates(directory="templates")
 
 # ====== PostgreSQL 資料庫配置 ======
 import os
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST","")
+DB_PORT = os.getenv("DB_PORT","")
+DB_NAME = os.getenv("DB_NAME","")
+DB_USER = os.getenv("DB_USER","")
+DB_PASSWORD = os.getenv("DB_PASSWORD","")
 DATABASE_URL = f"dbname={DB_NAME} user={DB_USER} password={DB_PASSWORD} host={DB_HOST} port={DB_PORT}"
 
 @contextmanager
